9fe11b9721e44655664e3c9789a16a9bbbfa6425
unknown <clsullivan@csustudent.net>
2015-03-24 23:48:49 -0400

    lab1

---
+++ src/lab12/ArrayList.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab12;
+
+/**
+ *
+ * @author csullivan156711
+ */
+public class ArrayList implements List{
+Object[] array1 = new Object[5];
+private Object temp; 
+    @Override
+    public Object get(int index) {
+        for(int i = 0; i < index; i++){
+        temp = Object[i];
+            
+        }
+        
+        
+       return null;
+    }
+
+    @Override
+    public void add(Object obj) {
+        
+       
+    }
+
+    @Override
+    public int size() {
+      return 0;
+    }
+     
+    
+}
---
+++ src/lab12/SLinkedList.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab12;
+
+/**
+ *
+ * @author csullivan156711
+ */
+public class SLinkedList implements List{
+    Node head, tail; 
+private int numsize;
+    @Override 
+    public Object get(int index) { 
+       Node rover = head;
+        
+        for(int i =0; i< index ;i++){
+          rover =  rover.next;
+        }
+         return rover.value ; 
+
+}
+  @Override 
+     public void add(Object obj) { 
+         Node node = new Node(); 
+         node.value = obj; 
+        if (head == null && tail == null) {     
+            node.next = null; 
+            head = tail = node; 
+        } else { 
+            // We assume both head tail are not null. 
+           if (head != null && tail != null) { 
+              node.next = null; 
+               tail.next = node; 
+                 tail = node; 
+             } else { 
+                 // Show never happen... 
+                System.err.println("Invalid state only one is null: head, tail"); 
+           } 
+       } 
+   } 
+
+ 
+    @Override 
+     public int size() { 
+        Node rover = head;
+        if (head != null){
+        numsize = 0;
+        }
+        while (rover.next != null ){
+            
+         rover =  rover.next;
+         numsize = numsize + 1;
+        }
+         
+        return numsize; 
+  } 
+     class Node { 
+        Object value; 
+         Node next; 
+     } }
