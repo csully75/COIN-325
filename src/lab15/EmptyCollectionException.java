615d4b90e3da263a0e202ca075514bb3fa02205a
unknown <clsullivan@csustudent.net>
2015-04-02 23:41:19 -0400

    Lab15

---
+++ src/lab15/EmptyCollectionException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab15;
+
+/**
+ *
+ * @author Sully
+ */
+public class EmptyCollectionException extends Exception {
+
+    EmptyCollectionException(String stack) {
+        
+    }
+  
+}
